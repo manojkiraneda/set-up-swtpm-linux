9c9949f066bbaa25d3efdb69e9674a10cc229eb1
Manojkiran Eda <manojkiran.eda@gmail.com>
2025-01-27 14:03:40 +0530

    Add initial version of script to install qemu
    Signed-off-by: Manojkiran Eda <manojkiran.eda@gmail.com>

---
+++ set-up-qemu-openbmc.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+
+# Install required dependencies
+sudo apt install git-email\
+	         libaio-dev \
+		 libbluetooth-dev \
+		 libcapstone-dev \
+		 libbrlapi-dev \
+		 libbz2-dev \
+		 libcap-ng-dev \
+		 libcu \
+		 rl4-gnutls-dev \
+		 libgtk-3-dev \
+		 libibverbs-dev \
+		 libjpeg8-dev \
+		 libncurses5-dev \
+		 libnuma-dev \
+		 librbd-dev \
+		 librdmacm-dev \
+		 libsasl2-dev \
+		 libsdl2-dev \
+		 libseccomp-dev \
+		 libsnappy-dev \
+		 libssh-dev \
+		 libvde-dev \
+		 libvdeplug-dev \
+		 libvte-2.91-dev \
+		 libxen-dev \
+		 liblzo2-dev \
+		 valgrind \
+		 xfslibs-dev
+
+# Download openbmc/qemu source code
+git clone https://github.com/openbmc/qemu.git ~/openbmc-qemu
+cd ~/openbmc-qemu
+./configure --target-list=arm-softmmu
+ninja -C build
+
+
